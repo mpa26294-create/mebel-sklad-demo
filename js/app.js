c0738c837fcdf49725f3bb9654bbb1608f54145a
MARIS <mpa26294@gmail.com>
2026-07-07 12:34:04 +0300

    Extract history core into history.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // FurniCore app entry file.
 // Основной код пока остаётся в index.html; сюда постепенно переносим модули.
 
-const FURNICORE_BUILD_VERSION = "v5.93.0 - Orders Modals";
+const FURNICORE_BUILD_VERSION = "v5.93.1 - History Core";
 
 function applyBuildVersion() {
   const badge = document.getElementById("appVersionBadge");
