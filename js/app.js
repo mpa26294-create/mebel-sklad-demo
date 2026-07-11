c78400c557a603ce8ddb2949d33e91a7ed970171
MARIS <mpa26294@gmail.com>
2026-07-11 10:44:59 +0300

    Add roll stock length and width adjustment

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // FurniCore app entry file.
 // Основной код пока остаётся в index.html; сюда постепенно переносим модули.
 
-const FURNICORE_BUILD_VERSION = "v6.19 - Production Material Warnings";
+const FURNICORE_BUILD_VERSION = "v6.20 - Roll Stock Adjustments";
 
 function applyBuildVersion() {
   const badge = document.getElementById("appVersionBadge");
